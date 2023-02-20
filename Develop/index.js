a3dcaad6ed69f51630a343a7abddb3a68dfe1387
bensboto <bensboto@gmail.com>
2023-02-20 13:34:40 -0700

    reinstalled inquirer package so it works properly

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // WHEN I enter my project title
 // THEN this is displayed as the title of the README
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
@@ -15,7 +15,11 @@
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "1. What is your project title?", "2. ", "3. What license will you be using?", "4. What is your Github username?", "5. What is your email address?"
+  "1. What is your project title?",
+  "2. ",
+  "3. What license will you be using?",
+  "4. What is your Github username?",
+  "5. What is your email address?",
 ];
 
 // TODO: Create a function to write README file
@@ -23,9 +27,76 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    for (let i = 0; i < questions.length; i++) {
-        console.log(questions[i]);
-    }
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is your project title?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Enter a project description",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Enter installation instructions",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information",
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "Enter contribution guidelines",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Enter test instructions",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Choose a license for your application",
+        choices: ["MIT", "Apache", "GPL", "Unlicensed"],
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+    ])
+    .then((answers) => {
+      const readmeData = {
+        title: answers.title,
+        description: answers.description,
+        installation: answers.installation,
+        usage: answers.usage,
+        contribution: answers.contribution,
+        test: answers.test,
+        license: answers.license,
+        author: {
+          name: answers.name,
+          username: answers.username,
+          email: answers.email,
+        },
+      };
+
+      console.log("Generated README data:", readmeData);
+    })
+    .catch((error) => {
+      console.log("There was an error:", error);
+    });
 }
 
 // Function call to initialize app
